890f488e3ca666bfb34c59b7163f8b57a6f0d049
46942 <469423491@qq.com>
2019-11-03 21:08:24 +0800

    修改代码

--- src/main/java/com/org/devg/elasticsearch/EsAutoConfiguration.java
+++ src/main/java/com/org/devg/elasticsearch/EsAutoConfiguration.java
@@ -1,9 +1,25 @@
 package com.org.devg.elasticsearch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author devg
  * @Date 2019/10/13 17:46
  */
+
+@Slf4j
+@Configuration
+@ConditionalOnWebApplication //web应用才生效
 public class EsAutoConfiguration {
+
+  @Bean
+  public EsSearch esSearch() {
+    EsSearch esSearch = new EsSearch();
+    log.info("init esSearch sucessful");
+    return esSearch;
+  }
 
 }
--- src/main/java/com/org/devg/elasticsearch/EsConfig.java
+++
@@ -1,9 +0,0 @@
-package com.org.devg.elasticsearch;
-
-/**
- * @author devg
- * @Date 2019/10/13 17:46
- */
-public class EsConfig {
-
-}
